bd66d27d9973d979540acbe193fd1a935412976f
Jasur Sulaymonov <75416890+Jarus95@users.noreply.github.com>
2024-01-09 15:22:07 +0500

    DATA: Create & Migrate Employees

---
+++ BarakaTexnika.Api/Brokers/Storages/StorageBroker.Employees.cs
@@ -0,0 +1,10 @@
+﻿using BarakaTexnika.Api.Employees;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarakaTexnika.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ BarakaTexnika.Api/Migrations/20240109102004_employeer_migrate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarakaTexnika.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class employeer_migrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    JobTitle = table.Column<string>(type: "TEXT", nullable: true),
+                    Salary = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
